14ad190cc5080cc152cdc729d4115b272e8ef4ec
Tim Kuhlbusch <tim.kuhlbusch@rwth-aachen.de>
2021-12-30 15:22:30 +0100

    hide the disclaimer on user defined namespaces through JS and some design adjustments

--- conf/default.php
+++ conf/default.php
@@ -2,4 +2,4 @@
 
 $conf['disclaimer_msg'] = 'Default disclaimer message, please change in configuration settings!';
 $conf['disclaimer_namespaces'] = '*';
-$conf['disclaimer_bg_color'] = 'yellow';
+$conf['disclaimer_bg_color'] = '#ff6';
--- script.js
+++ script.js
@@ -6,12 +6,22 @@
 
 // go through all disclaimer boxes in the document
 var boxes = document.getElementsByClassName("disclaimer_box");
-for(var idx=0; idx<boxes.length; idx++) {
-    boxes[idx].innerHTML = NS;
+
+// going backwords because remove() seems to affect the length of boxes
+for(var idx=(boxes.length-1); idx>=0; idx--) {
+    var target_namespace = boxes[idx].dataset.disclaimerNs;
+    console.debug(idx, "Target Namespace", target_namespace);
 
     // remove if not in main body
     if (boxes[idx].parentNode.id != "bodyContent") {
         boxes[idx].remove();
+        continue;
+    }
+
+    // remove if we are not in one of the selected namespaces
+    if ( (target_namespace != "*") && (!target_namespace.split(',').includes(NS)) ) {
+        boxes[idx].remove();
+        continue;
     }
 }
 
